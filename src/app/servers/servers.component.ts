50fe187c22cbf4256f795f13828bf9a0ca6449a8
Osmand Das <osmandpdas@gmail.com>
2018-09-30 23:55:39 -0400

    updated code

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-servers',
@@ -9,10 +10,17 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   private servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, private router: Router, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
+  }
+
+  onReload(){
+    // if we put just the 'servers' instead of '/servers' then it will try to load /servers/servers
+    // which is an invalid for our app
+    // activateRoute is the route which loaded this page
+    this.router.navigate(['/servers'], { relativeTo: this.activatedRoute})
   }
 
 }
